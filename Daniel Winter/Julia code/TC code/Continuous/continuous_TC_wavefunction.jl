0449e0e19282239c11c723baec98e7c7345e011b
Dwinter-44 <77338436+Dwinter-44@users.noreply.github.com>
2021-05-27 12:24:13 +0100

    ZString is different ??

--- Daniel Winter/Julia code/TC code/Continuous/continuous_TC_wavefunction.jl
+++ Daniel Winter/Julia code/TC code/Continuous/continuous_TC_wavefunction.jl
@@ -1,14 +1,28 @@
 using Yao
+#using Random
+using Plots
+using FFTW
+using Distributions
+theme(:dao)
 
 N = 6
 Xstring = chain(N, prod([put(N, i=>X) for i=1:N]))
+Ystring = chain(N, prod([put(N, i=>Y) for i=1:N]))
+Zstring = chain(N, prod([put(N, i=>Z) for i=1:N]))
 ZZpairs = sum([chain(N, put(N, i=>Z)*put(N, i+1=>Z)) for i=1:N-1])
 
-U(Jt::Float64) = time_evolve(0.5 * Xstring + 0.5 * ZZpairs, Jt, tol=1e-5, check_hermicity=true)
+U(Jt::Float64) = time_evolve(0.5 * Xstring + 0.0 * Ystring + 0.0 * Zstring + 0.5 * ZZpairs, Jt, tol=1e-15, check_hermicity=true)
 Mz(N::Int) = sum([put(N, i => Z) for i = 1:N]) / N
 
-function Mz_evolve(nsteps, deltaJt)
-    protected = true
+macro Name(arg)
+   string(arg)
+end
+
+plot_name = @Name Continuous_TC_modwave
+
+protected = true
+
+function Mz_evolve(N::Int, nsteps::Int64, deltaJt)
     t_vec = Vector{Float64}();
     Mz_vec = Vector{Float64}();
     ψ = zero_state(N)
@@ -24,10 +38,50 @@ function Mz_evolve(nsteps, deltaJt)
     return t_vec, Mz_vec
 end
 
-using Plots
-title = "" *string(plot_name)* " plot for "* string(N) * " Qubits DeltaJt = " * string(0.00001)
-t_vec, Mz_vec = Mz_evolve(10000, 0.00001)
-plt=Plots.plot(t_vec[9000:10000], Mz_vec[9000:10000], xlabel = "Time/ period of driving field", ylabel = " \n"*"Magnetisation / fraction of maximum value\n and orientation", legend = false)
-Plots.title!(title)
-display(plt)
+
+function save_plot(name, N, step, plot, label)
+    cd(dirname(@__FILE__));
+    dir = pwd();
+    println("Saving plots to the directory in $dir","\\Graphs")
+    strlabel = string(label);
+    mkpath(string(dir,"\\Graphs\\","\\",name,"\\","\\",name,"_",N, " qubits\\","\\Protected ",protected,"\\","\\",step," steps"))
+    pngfilename = string(dir,"\\Graphs\\","\\",name,"\\","\\",name,"_",N, " qubits\\","\\Protected ",protected,"\\","\\",step," steps\\",strlabel,".png")
+    #pdffilename = string(dir,"\\Graphs\\","\\",name,"_",q, " qubits\\","\\",step," steps\\",strlabel,".pdf")
+    #epsfilename = string(dir,"\\Graphs\\","\\",name,"_",q, " qubits\\","\\",step," steps\\",strlabel,".eps")
+    savefig(plot, pngfilename)
+    #savefig(plot, pdffilename)
+    #savefig(plot, epsfilename)
+end
+
+for j = N # Number of qubits for each
+    for step = 10000
+        figpath1 = "C:/Users/Daniel/OneDrive/Documents/Exeter Uni/Modules/Year 3/Project-Time crystals/Julia Code/Graphs/" *plot_name* " " *string(j)* " qubits/" * string(step) * " steps/"
+        # 1 after variable names denote they're local variables in the for loop
+        # And here is where the file path is defined for each iteration.
+        for i = 0.00001
+            title1 = "" *string(plot_name)* " plot for "* string(j) * " Qubits Jt = " * string(i)
+            t_vec1, Mz_vec1 = Mz_evolve(j, step, i)
+            fig=Plots.plot(t_vec1[1:10000], Mz_vec1[1:10000], linetype=:steppre, xlabel = "Time/ period of driving field", ylabel = " \n"*"Magnetisation / fraction of maximum value\n and orientation", legend = false)
+            Plots.title!(title1)
+            #Plots.savefig(figpath1*title1*".png") #Saves the plots onto my computer but requires me to make a folder
+
+            label=title1;
+
+            save_plot(plot_name, j, step, fig, label) # Saves the plots to github
+            display(fig)
+
+            tit1 = "FFT of "*string(plot_name)* " plot for "* string(j) * " Qubits for Jt = " * string(round(i/(10*pi), digits=2))*" π"
+            Mz_vec_fft1 = fft(Mz_vec1)
+            fourier=Plots.plot(abs.(Mz_vec_fft1), linetype=:steppre, xlabel="Frequecy", xlims = (0, step), ylabel="Intensity", legend = false)
+            Plots.title!(tit1)
+            #display(fourier)
+            lab="FFT of "*string(plot_name)* " plot for "* string(j) * " Qubits for Jt = " * string(round(i/(10*pi), digits=2))*" π";
+
+            #save_plot(plot_name, q, step, fourier, lab) # Saves the plots to github
+        end
+        println("Successfully finished "*string(step)*" steps\n")
+    end
+    println("Successfully finished "*string(j)*" qubits\n")
+end
+
 println("Successfully finished")
