027626c0e45d430bb47526bc55bf1caab594c42f
Dwinter-44 <77338436+Dwinter-44@users.noreply.github.com>
2021-05-31 16:46:34 +0100

    chaos pic??

---
+++ Daniel Winter/Julia code/TC code/Cavity_paper_examples.jl
@@ -0,0 +1,91 @@
+using Yao
+using Plots
+using FFTW
+theme(:dao)
+
+println("Successfully started from beginnig")
+
+
+N=6
+Zstring = chain(N, prod([put(N, i=>Z) for i=1:N]))
+RXstr(N::Int) = chain(N, prod([put(N, i=>Rx(0.05)) for i=1:N]))
+
+U(Jt::Float64) = time_evolve(Zstring, Jt, tol=1e-5, check_hermicity=true)
+Mz(N::Int) = sum([put(N, i => Z) for i = 1:N]) / N
+
+macro Name(arg)
+   string(arg)
+end
+
+plot_name = @Name Cavity_paper_ex
+
+protected = true
+
+function Mz_evolve(N::Int, nsteps::Int64, deltaJt)
+    t_vec = Vector{Float64}();
+    Mz_vec = Vector{Float64}();
+    ψ = zero_state(N)
+    for i = 0:nsteps
+        append!(t_vec, i * deltaJt)
+        if protected
+            ψ |> U(i * deltaJt) |> RXstr(N) |> U(i * deltaJt)
+        else
+            ψ |> U(i * deltaJt) |> RXstr(N) |> U(i * deltaJt)
+        end
+        append!(Mz_vec, expect(Mz(N), ψ))
+    end
+    return t_vec, Mz_vec
+end
+
+
+function save_plot(name, N, step, plot, label)
+    cd(dirname(@__FILE__));
+    dir = pwd();
+    println("Saving plots to the directory in $dir","\\Graphs")
+    strlabel = string(label);
+    mkpath(string(dir,"\\Graphs\\","\\",name,"\\","\\",name,"_",N, " qubits\\","\\Protected ",protected,"\\","\\",step," steps"))
+    pngfilename = string(dir,"\\Graphs\\","\\",name,"\\","\\",name,"_",N, " qubits\\","\\Protected ",protected,"\\","\\",step," steps\\",strlabel,".png")
+    #pdffilename = string(dir,"\\Graphs\\","\\",name,"_",q, " qubits\\","\\",step," steps\\",strlabel,".pdf")
+    #epsfilename = string(dir,"\\Graphs\\","\\",name,"_",q, " qubits\\","\\",step," steps\\",strlabel,".eps")
+    savefig(plot, pngfilename)
+    #savefig(plot, pdffilename)
+    #savefig(plot, epsfilename)
+end
+
+1.0e6
+
+for j = N # Number of qubits for each
+    low= 1
+    hig = 1000000
+    rang=low:hig
+    for step = 1000000
+        figpath1 = "C:/Users/Daniel/OneDrive/Documents/Exeter Uni/Modules/Year 3/Project-Time crystals/Julia Code/Graphs/" *plot_name* " " *string(j)* " qubits/" * string(step) * " steps/"
+        # 1 after variable names denote they're local variables in the for loop
+        # And here is where the file path is defined for each iteration.
+        for i = 1.0e-5
+            title1 = "" *string(plot_name)* " plot for "* string(j) * " Qubits for Jt = " *string(i)
+            t_vec1, Mz_vec1 = Mz_evolve(j, step, i)
+            plt=Plots.plot(t_vec1[rang], Mz_vec1[rang], linetype=:steppre, xlabel = "Time/ period of driving field", ylabel = " \n"*"Magnetisation / fraction of maximum value\n and orientation", legend = false)
+            Plots.title!(title1)
+            #Plots.savefig(figpath1*title1*".png") #Saves the plots onto my computer but requires me to make a folder
+
+            label="" *string(plot_name)* " plot for "* string(j) * " Qubits between " *string(low)* " and " *string(hig);
+
+            save_plot(plot_name, j, step, plt, label) # Saves the plots to github
+            display(plt)
+
+            #tit1 = "FFT of "*string(plot_name)* " plot for "* string(j) * " Qubits for Jt = " * string(i)
+            #Mz_vec_fft1 = fft(Mz_vec1)
+            #fourier=Plots.plot(abs.(Mz_vec_fft1), linetype=:steppre, xlabel="Frequecy", xlims = (0, step), ylabel="Intensity", legend = false)
+            #Plots.title!(tit1)
+            #display(fourier)
+            #lab="FFT of "*string(plot_name)* " plot for "* string(j) * " Qubits for Jt = " * string(i);
+
+            #save_plot(plot_name, j, step, fourier, lab) # Saves the plots to github
+        end
+        println("Successfully finished "*string(step)*" steps\n")
+    end
+    println("Successfully finished "*string(j)*" qubits\n")
+end
+
+println("Successfully finished")
---
+++ Daniel Winter/Julia code/TC code/Continuous/continuous_TC_sim_OK.jl
@@ -0,0 +1,140 @@
+using Yao
+
+S=sqrt(Z)
+
+N = 6
+Xstring = chain(N, prod([put(N, i=>X) for i=1:N]))
+ZZpairs = sum([chain(N, put(N, i=>Z)*put(N, i+1=>Z)) for i=1:N-1])
+
+U(Jt::Float64) = time_evolve(0.5 * Xstring + 0.5 * ZZpairs, Jt, tol=1e-5, check_hermicity=true)
+Mz(N::Int) = sum([put(N, i => Z) for i = 1:N]) / N
+
+function Mz_evolve(nsteps, deltaJt)
+    protected = true
+    t_vec = Vector{Float64}();
+    Mz_vec = Vector{Float64}();
+    ψ = zero_state(N)
+    for i = 0:nsteps
+        append!(t_vec, i * deltaJt)
+        if protected
+            ψ |> U(i * deltaJt) 
+        else
+            ψ |> U(i * deltaJt)
+        end
+        append!(Mz_vec, expect(Mz(N), ψ))
+    end
+    return t_vec, Mz_vec
+end
+
+using Plots
+t_vec, Mz_vec = Mz_evolve(10000, 0.00001)
+plot(t_vec[9000:10000], Mz_vec[9000:10000])
+
+
+# check if the operators commute
+iscommute(Xstring, ZZpairs)
+
+
+# implement the sequence of commuting ZZ evolutions as CNOT - Rz - CNOT
+function zz_evolve(n::Int, angle::Float64)
+    cirq = chain(n)
+    for i=1:n-1
+        # implement ZZ(θ) unitaries
+        push!(cirq, chain(n, cnot(i,i+1)))
+        push!(cirq, chain(n, put((i+1)=>Rz(2*angle))))
+        push!(cirq, chain(n, cnot(i,i+1)))
+    end
+    return cirq
+end
+
+function xstr_evolve(n::Int, angle::Float64)
+    cirq = chain(n)
+    # implement XX..X(θ) string
+    push!(cirq, repeat(H, 1:n))
+    push!(cirq, chain(n, cnot(i,i+1) for i=1:n-1))
+    push!(cirq, chain(n, put(n=>Rz(2*angle))))
+    push!(cirq, chain(n, cnot(i,i+1) for i=1:n-1)')
+    push!(cirq, repeat(H, 1:n))
+    return cirq
+end
+
+
+# do some tests
+using Random
+Jt = rand()
+psi = rand_state(N)
+psi1 = copy(psi) |> zz_evolve(N, Jt)
+psi2 = copy(psi) |> time_evolve(ZZpairs, Jt, tol=1e-10, check_hermicity=true)
+fidelity(psi1, psi2) #fidelity is the measure of the distance between two quantum states.
+
+Zstring = chain(N, prod([put(N, i=>Z) for i=1:N]))
+
+Jt = rand()
+psi = rand_state(N)
+psi1 = copy(psi) |> xstr_evolve(N, Jt)
+psi2 = copy(psi) |> time_evolve(Xstring, Jt, tol=1e-10, check_hermicity=true)
+fidelity(psi1, psi2)
+
+
+function fast_Mz_evolve(nsteps, deltaJt)
+    t_vec = Vector{Float64}();
+    Mz_vec = Vector{Float64}();
+    ψ = zero_state(N)
+    for i = 0:nsteps
+        append!(t_vec, i * deltaJt)
+        ψ |> xstr_evolve(N, 0.5*i * deltaJt) |> zz_evolve(N, 0.5*i * deltaJt)
+        append!(Mz_vec, expect(Mz(N), ψ))
+    end
+    return t_vec, Mz_vec
+end
+
+t_vec_fast, Mz_vec_fast = fast_Mz_evolve(10000, 0.00001)
+plot(t_vec_fast[9000:10000], Mz_vec_fast[9000:10000])
+
+
+function pauli_string_exp(n, dict, theta)
+    cirq = chain(n)
+    dict_keys = [key for key in keys(dict)]
+
+    for key in dict_keys
+        if dict[key] == "X"
+            push!(cirq, put(n, key=>H)  )
+        elseif dict[key] == "Y"
+            push!(cirq, put(n, key=>S')  )
+            push!(cirq, put(n, key=>H)  )
+        end
+    end
+
+    for i_key in 2:length(dict_keys)
+        push!(cirq, cnot(n, dict_keys[i_key - 1], dict_keys[i_key]))
+    end
+
+    push!(cirq, chain(n, put(dict_keys[end]=>Rz(theta))) )
+
+    for i_key in length(dict_keys):-1:2
+        push!(cirq, cnot(n, dict_keys[i_key - 1], dict_keys[i_key]))
+    end
+
+    for key in dict_keys
+        if dict[key] == "X"
+            push!(cirq, put(n, key=>H)  )
+        elseif dict[key] == "Y"
+            push!(cirq, put(n, key=>H)  )
+            push!(cirq, put(n, key=>S)  )
+        end
+    end
+
+    return cirq
+end
+d = Dict(1=>"X", 2=>"Y", 3=>"X", 4=>"X", 5=>"X", 6=>"X");
+d[2]
+d_keys = [key for key in keys(d)]
+
+pauli_string_exp(6, d, 0.1)
+
+Jt = rand()
+psi = rand_state(N)
+psi1 = copy(psi) |> xstr_evolve(N, Jt)
+#psi2 = copy(psi) |> time_evolve(Xstring, Jt, tol=1e-10, check_hermicity=true)
+psi2 = copy(psi) |> pauli_string_exp(N, d, 2*Jt)
+fidelity(psi1, psi2)
