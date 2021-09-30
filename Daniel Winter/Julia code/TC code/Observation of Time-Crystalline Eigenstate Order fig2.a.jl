665a1a5f56cf2648b405ae5e6b472c0b7cbb2b59
Dwinter-44 <77338436+Dwinter-44@users.noreply.github.com>
2021-09-30 13:51:38 +0100

    Update Observation of Time-Crystalline Eigenstate Order fig2.a.jl

--- Daniel Winter/Julia code/TC code/Observation of Time-Crystalline Eigenstate Order fig2.a.jl
+++ Daniel Winter/Julia code/TC code/Observation of Time-Crystalline Eigenstate Order fig2.a.jl
@@ -10,6 +10,8 @@ ZZpairs = sum([chain(N, put(N, i=>Z)*put(N, i+1=>Z)) for i=1:N-1])
 Xrotstring = chain(N, prod([put(N, i=>Rx(0.05)) for i=1:N]))
 
 U(Jt::Float64) = time_evolve(0.5 * Zstring + 0.5 * ZZpairs + 0.5 * Xrotstring, Jt, tol=1e-5, check_hermicity=true)
+#U(Jt::Float64) = time_evolve(0.5 * Zstring + 0.5 * ZZpairs, Jt, tol=1e-5, check_hermicity=true)
+
 Mz(N::Int) = sum([put(N, i => Z) for i = 1:N]) / N
 
 macro Name(arg)
@@ -52,4 +54,4 @@ function save_plot(name, q, step, plot, label)
 end
 
 t_vec, Mz_vec = Mz_evolve(10000, 0.00001)
-plot(t_vec[9000:10000], Mz_vec[9000:10000])
+plot(t_vec[1:10000], Mz_vec[1:10000])
