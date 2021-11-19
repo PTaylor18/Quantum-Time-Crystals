cf0a8220d5d9e3970c83dd7e2353578f73b40d56
Kieron25 <78971973+Kieron25@users.noreply.github.com>
2021-11-19 12:03:49 +0000

    Add files via upload
    This is the latest version of the code to determine the Green's Function of the PXP Hamiltonian model.

--- Kieron Lintott/Correlation function open boundary.jl
+++ Kieron Lintott/Correlation function open boundary.jl
@@ -97,35 +97,47 @@ function op(::OpName"openPXPop",::SiteType"S=1/2", s1::Index, s2::Index, s3::Ind
          op("Z", s1)*op("X", s2)*op("I", s3)
   return exp(τ * ampo)
 end
-
+#=
 function excitation_op(N, site)
   a = site - 1
   b = site + 1
-  P = otimes(op("I", sites[2]), op("I", sites[1]))
-  for i = 3:a
-    P = otimes(op("I", sites[i]), P)
+  P =  [op("I", sites[1])]
+  for i = 2:a
+    append!(P, [op("I", sites[i])])
   end
-  Px = otimes(op("X", sites[site]), P)
+  append!(P, [op("X", sites[site])])
   # And then apply identity operators on the rest of the sites.
-  for i = b:N
-    Px = otimes(op("I", sites[i]), P)
+  for i = b:(N-2)
+    append!(P, [op("I", sites[i])])
   end
-  display(Px)
-  #println(typeof(Px)) # An ITensor object
-  return Px
+  #display(P)
+  #println(typeof(P)) # An ITensor object
+  return P
 end
+=#
+function excite(N, site)
+    ampo = AutoMPO()
+    ampo += "S+",site
+    return ampo
+end
+#excite(N, 4)
+
+function deexcite(N, site)
+    ampo = AutoMPO()
+    ampo += "S-",site
+    return ampo
+end
+#deexcite(N, 4)
 
 H = Hpxp_open_DMRG(sites, N)
 state = [isodd(n) ? "Up" : "Dn" for n=1:N]
-psi0 = productMPS(sites,state)
+psi = productMPS(sites,state)
 
 sweeps = Sweeps(7)
 maxdim!(sweeps, 30,60,150,140,100)
 cutoff!(sweeps, 1E-8)
 
-energy, psi1 = dmrg(H,psi0, sweeps)
-
-Nsteps = 5/t
+energy, psi0 = dmrg(H,psi, sweeps)
 
 function CdagexpCexp(ψ, s, t, N, site)
   #=
@@ -136,46 +148,116 @@ function CdagexpCexp(ψ, s, t, N, site)
   real quantity), N is the number of sites and site is the location of the
   excitation in the chain.
   =#
-  A = excitation_op(N, site)
+  #A = MPO(deexcite(N, site), s)
+  A = op("S-", s[site])
+  #A = dexcite(N, site)
   B = ops([("openPXPop", (n, n + 1, n + 2), (τ=-t * im / 2,)) for n in 1:(N - 2)], s)
-  gates = A * B
-  append!(gates, reverse(gates))
-  return gates
+  Bdag = ops([("openPXPop", (n, n + 1, n + 2), (τ=t * im / 2,)) for n in 1:(N - 2)], s)
+  #Adag = MPO(excite(N, site), s)
+  Adag = op("S+", s[site])
+
+  Nsteps = 5/t
+  #ρ0 = MPO(ψ)
+  ρ0 = ψ
+  ρ1 = ρ0
+  τ = t
+  for step in 1:Nsteps
+    ρ1 = apply(B, ρ1)
+    τ += t
+  end
+  #println("Got to here")
+  orthogonalize!(ρ1, site)
+  newA = A * ρ1[site]
+  noprime!(newA)
+  ρ1[site] = newA
+  #println("Got to here too")
+  for step in 1:Nsteps
+    ρ1 = apply(Bdag, ρ1)
+    #τ += t
+  end
+  #println("Got to here as well")
+  orthogonalize!(ρ1, site)
+  newAdag = Adag * ρ1[site]
+  noprime!(newAdag)
+  ρ1[site] = newAdag
+  #println("Almost done it")
+  ρ = outer(ρ1, ρ1)
+  #println("\n", inner(ψ, ρ, ψ))
+  return inner(ψ, ρ, ψ)
 end
 
-function expCexpCdag(ψ, s, t)
-#=
-This function creates the operator described and calculates the expectation
-value of the operator on the ground state, in order to get the Green's function
-of the system.
-=#
-  B = excitation_op(N, site)
-  A = ops([("openPXPop", (n, n + 1, n + 2), (τ=t * im / 2,)) for n in 1:(N - 2)], s)
-  gates = A * B
-  append!(gates, reverse(gates))
-  return gates
+function expCexpCdag(ψ, s, t, N, site, totalt=5)
+  #=
+  This function creates the operator described and calculates the expectation
+  value of the operator on the ground state, in order to get the Green's function
+  of the system.
+  =#
+  Bdag = op("S+", s[site])
+  #B = excite(N, site)
+  Adag = ops([("openPXPop", (n, n + 1, n + 2), (τ=t * im / 2,)) for n in 1:(N - 2)], s)
+  A = ops([("openPXPop", (n, n + 1, n + 2), (τ=-t * im / 2,)) for n in 1:(N - 2)], s)
+  B = op("S-", s[site])
+
+  Nsteps = totalt/t
+  #ρ0 = MPO(ψ)
+  ρ0 = ψ
+  ρ1 = ρ0
+  τ = t
+  orthogonalize!(ρ1, site)
+  newBdag = Bdag * ρ1[site]
+  noprime!(newBdag)
+  ρ1[site] = newBdag
+  #println("step 1")
+  for step in 1:Nsteps
+    ρ1 = apply(A, ρ1)
+    τ += t
+  end
+  #println("step 2")
+  orthogonalize!(ρ1, site)
+  newB = B * ρ1[site]
+  noprime!(newB)
+  ρ1[site] = newB
+  #println("step 3")
+  for step in 1:Nsteps
+    ρ1 = apply(Adag, ρ1)
+    τ += t
+  end
+  #println("step 4")
+  ρ = outer(ρ1, ρ1)
+  #println("\n", inner(ψ, ρ, ψ))
+  return inner(ψ, ρ, ψ)
+end
+
+
+function totalGreensfunction(ψ, s, t, N, site)
+  #=
+  See equation referenced in my lab book. gates1 and gates2 are expCexpCdag
+  and CdagexpCexp
+  =#
+  G = CdagexpCexp(ψ, s, t, N, site) - im * expCexpCdag(ψ, s, t, N, site)
+  #println(G)
+  return G
 end
 #=
 # Make gates (1,2,3),(2,3,4),(3,4,5),...
 gates = ops([("openPXPop", (n, n + 1, n + 2), (τ=-t * im / 2,)) for n in 1:(N - 2)], sites)
 
 # This is the time evolution operator with the exponent of the Hamiltonian,
-# we also want the excitation of
+# we also want the excitation of a specific site leaving all others the same
 
 # Include gates in reverse order too
 # (N,N-1),(N-1,N-2),...
 append!(gates, reverse(gates))
 #display(gates)
-# Note: I think you need to
 
-psi = psi1
+psi = psi0
 for step in 1:Nsteps
     psi = apply(gates, psi; cutoff=cutoff)
     τ += t
     # measure an observable
 end
 
-rho0 = outer(psi1, psi1)
+rho0 = outer(psi0, psi0)
 rho = rho0
 for step in 1:Nsteps
   rho = apply(gates, rho; cutoff=cutoff, apply_dag=true)
@@ -187,5 +269,14 @@ end
 
 
 =#
-CdagexpCexp(psi1, sites, t, N, 5)
+#CdagexpCexp(psi0, sites, t, N, 3)
+#totalGreensfunction(psi0, sites, t, N, 3)
+
+Garr = [1.00 + 0.00*im]
+for tl in 0.01:0.01:0.1 # tl is short for time local (to this loop in the code)
+  G = totalGreensfunction(psi0, sites, tl, N, 3)
+  push!(Garr, G)
+end
+println([0:0.01:0.1], Garr)
+
 println("Done!")
